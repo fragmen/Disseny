86dfc609e8dc36e7d83448058a3aa55b3a84f31e
ferran <fmassafrt@gmail.com>
2018-02-27 12:57:39 +0100

    drag and drop vocals

---
+++ js/drag_drop.js
@@ -0,0 +1,40 @@
+$(function () {
+    
+});
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    
+    var targetId = ev.target.id;
+    var objectId = ev.dataTransfer.getData("text");
+    
+    var objectVocal = $("#" + objectId).attr("vocal");
+    var targetVocal = $("#" + targetId).attr("vocal");
+
+    console.log(targetId);
+    console.log(objectId);
+    console.log(objectVocal);
+    console.log(targetVocal);
+    
+    if(objectVocal == targetVocal){
+        ev.target.appendChild(document.getElementById(objectId));
+    } 
+    else{
+        $("#" + targetId).css({border: ' 2px solid red'});
+        setTimeout(function(){
+            $("#" + targetId).css({border: ' 1px solid red'});
+        }, 400);
+        setTimeout(function(){
+            $("#" + targetId).css({border: ' 1px solid black'});
+        }, 500);
+    }
+}
