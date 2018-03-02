5c4ba764e289034c239efe6e90026d4de02c6f41
Fragmen <fmassafrt@gmail.com>
2018-03-02 17:06:35 +0100

    merge

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -1,46 +1,13 @@
-<<<<<<< HEAD
-$(function () {
-    
-});
-
-
-=======
->>>>>>> d697c4d1979d9f28053da0baf59a792b4f9ec89f
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+    
+    $(".dropDiv").css({border: ' 2px dashed black'});
 }
 
-<<<<<<< HEAD
-function drop(ev) {
-    ev.preventDefault();
-    
-    var targetId = ev.target.id;
-    var objectId = ev.dataTransfer.getData("text");
-    
-    var objectVocal = $("#" + objectId).attr("vocal");
-    var targetVocal = $("#" + targetId).attr("vocal");
-
-    console.log(targetId);
-    console.log(objectId);
-    console.log(objectVocal);
-    console.log(targetVocal);
-    
-    if(objectVocal == targetVocal){
-        ev.target.appendChild(document.getElementById(objectId));
-    } 
-    else{
-        $("#" + targetId).css({border: ' 2px solid red'});
-        setTimeout(function(){
-            $("#" + targetId).css({border: ' 1px solid red'});
-        }, 400);
-        setTimeout(function(){
-            $("#" + targetId).css({border: ' 1px solid black'});
-        }, 500);
-=======
 
 function drop(ev) {
     ev.preventDefault();
@@ -52,26 +19,26 @@ function drop(ev) {
     var targetVocal = $("#" + targetId).attr("vocal");
 
     var targetClass = $("#" + targetId).attr("class");
-
-    console.log("#" + objectId);
-    console.log("#" + targetId);
-    console.log(objectVocal);
-    console.log(targetVocal);
-
-    console.log(targetClass);
-
+    
+    console.log("targetClass" + targetClass);
     if (targetClass == "dropDiv") {
         if (objectVocal == targetVocal) {
             ev.target.appendChild(document.getElementById(objectId));
+            $("#" + targetId).attr("class","dropDiv_");
+            
+            $(".dropDiv_").css({border: 'none'});
+            $(".dropDiv").css({border: 'none'});
+            
         } else {
-            $("#" + targetId).css({border: ' 2px solid red'});
+            $("#" + targetId).css({border: ' 2px dashed red'});
             setTimeout(function () {
-                $("#" + targetId).css({border: ' 1px solid red'});
-            }, 400);
+                $(".dropDiv").css({border: 'none'});
+            }, 1000);
             setTimeout(function () {
-                $("#" + targetId).css({border: ' 1px solid black'});
-            }, 500);
+                $("#" + targetId).css({border: ' none'});
+            }, 1500);
+            
+            
         }
->>>>>>> d697c4d1979d9f28053da0baf59a792b4f9ec89f
     }
 }
