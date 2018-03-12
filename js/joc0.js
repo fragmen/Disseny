f8e7b4c2b286c3205fbc05ac9944e2a9a75975da
ferran <fmassafrt@gmail.com>
2018-03-12 08:47:01 +0100

    js

---
+++ js/joc0.js
@@ -0,0 +1,6 @@
+//var dragimages = ["abella", "all", "arana", "escala", "espasa", "estrella", "illa", "imant", "olla","os", "ou", "ull", "ulleres"];
+//
+//
+//function addDragableItem(nom, vocal){
+//    var Item = "<img src='img/" + nom + ".png'   id='" + nom + "'class='imageDD' vocal='" + vocal + "'   ondragstart='drag(event)'>"
+//}
